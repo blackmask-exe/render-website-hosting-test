0263d6206f8d59382ade04c3792c421e0f556910
Pratham S <snehi.pratham@gmail.com>
2024-09-16 12:08:16 -0500

    try to fix getargspec error in render

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 import os
+
+import inspect
+
+if not hasattr(inspect, 'getargspec'):
+    inspect.getargspec = inspect.getfullargspec
+
+from invoke import task
+
+@task
+def say_hello(c):
+    print("Hello, World!")
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("FLASK_KEY")
 
